09a5fdcc9ac4a1a718ae9f4be8270481a09fd10a
Dilvane Zanardine <dilvanezanardine@gmail.com>
2020-07-19 13:30:34 +0200

    feat(add buymeacoffee): add button

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -23,11 +23,23 @@ export const Navbar = () => {
           flexDirection: "column",
           borderRight: "2px solid",
           borderColor: "gray.7",
+          mb: 3,
         }}>
         <Image
           src="static/TypesGenerator_DAYZ_White_300x300.png"
           sx={{ width: "200px", height: "200px" }}
         />
+        <a
+          className="bmc-button"
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://www.buymeacoffee.com/squadzteam">
+          <img
+            src="https://cdn.buymeacoffee.com/buttons/bmc-new-btn-logo.svg"
+            alt="Buy me a coffee"
+          />
+          <span>Buy us a coffee</span>
+        </a>
       </Flex>
     </Content>
   );
